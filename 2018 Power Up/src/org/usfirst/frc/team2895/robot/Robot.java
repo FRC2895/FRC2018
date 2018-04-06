29518fd07f2724892a631024e848608eee61f542
Joe Pokorny <team2895@outlook.com>
2018-04-06 17:13:57 -0400

    Added autonomous. Latest version during testing. works for driving and shooter, intake is X not Y.

--- 2018 Power Up/src/org/usfirst/frc/team2895/robot/Robot.java
+++ 2018 Power Up/src/org/usfirst/frc/team2895/robot/Robot.java
@@ -9,43 +9,83 @@ package org.usfirst.frc.team2895.robot;
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.Spark;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive; 
 
 public class Robot extends IterativeRobot {
 	private DifferentialDrive m_driveTrain;
-	private Joystick m_joyStick;
-	private DifferentialDrive m_intake;
-	private DifferentialDrive m_shooter;
+	private Joystick m_driveJoyStick;
+	private Joystick m_shooterJoyStick;
+	private Spark m_intake;
+	private Spark m_shooterR;
+	private Spark m_shooterL;
+	//private DifferentialDrive m_shooter;
 	
-	public static class ButtonMap {
-		public int A = 0;
-		public int B = 1;
-		public int X = 2;
-		public int Y = 3;
-		public int LB = 4;
-		public int RB = 5;
-		public int Back = 6;
-		public int Start = 7;
-		public int L3 = 8;
-		public int R3 = 9;
+	public class ButtonMap {
+		public static final int A = 0;
+		public static final int B = 1;
+		public static final int X = 2;
+		public static final int Y = 3;
+		public static final int LB = 4;
+		public static final int RB = 5;
+		public static final int Back = 6;
+		public static final int Start = 7;
+		public static final int L3 = 8;
+		public static final int R3 = 9;
 	}
 	
 	@Override
 	public void robotInit() {
 		m_driveTrain = new DifferentialDrive(new Spark(0), new Spark(1));
-		m_intake = new DifferentialDrive(new Spark(2), new Spark(3));
-		m_shooter = new DifferentialDrive(new Spark(4), new Spark(5));
-		m_joyStick = new Joystick(0);
+		m_intake = new Spark(2) ;
+		m_shooterR = new Spark(7);
+		m_shooterL = new Spark(8);
+		
+		//m_shooter = new DifferentialDrive(new Spark(7), new Spark(8));
+		m_driveJoyStick = new Joystick(0);
+		m_shooterJoyStick = new Joystick(1);
 	}
 	
 	@Override
-	public void autonomousPeriodic() {
-		super.autonomousPeriodic();
-		// TODO: Write Code for autonomous
+	public void disabledInit(){}
+	
+	@Override
+	public void teleopInit() {
 		
+	}
+
+	boolean AUTONOMOUS_COMPLETE = false;
+	
+	@Override
+	public void autonomousPeriodic() {
+		//super.autonomousPeriodic();
+		if (AUTONOMOUS_COMPLETE) { return; }
+
+		//  Disable so we can let run during Timer delay
+		m_driveTrain.setSafetyEnabled(false);
+		
+	    // Drive forward for 2 seconds
+		double speed = -0.60;
+		m_driveTrain.arcadeDrive(1.0*speed, 0);
+		Timer.delay(2.5);
+		
+		m_driveTrain.arcadeDrive(0, 0);
+		
+		// Re-enable motor safety shut off
+		m_driveTrain.setSafetyEnabled(true);
+		
+		// Full power shooter mechanism
+		m_shooterR.set(-1.0);
+		m_shooterL.set(-1.0);
+		
+		Timer.delay(10.0);
+		
+		// Stop shooter mechanism
+		m_shooterR.set(0);
+		m_shooterL.set(0);
+		
+		AUTONOMOUS_COMPLETE = true;
 	}
 
 	@Override
@@ -54,10 +94,27 @@ public class Robot extends IterativeRobot {
 		while (isOperatorControl() && isEnabled()) {
 			
 			// Drive arcade style
-			m_driveTrain.arcadeDrive(-m_joyStick.getY(), m_joyStick.getX());
+			m_driveTrain.arcadeDrive(-m_driveJoyStick.getY(), m_driveJoyStick.getX());
 			
 			// Intake
+//			if(m_shooterJoyStick.getRawButton(ButtonMap.LB)) // window motor on
+//			{
+//				m_intake.setSpeed(0.2);
+//			} else{
+//				m_intake.setSpeed(0);
+//			}
 			
+			m_shooterR.set(-1*m_shooterJoyStick.getY());
+			m_shooterL.set(-1*m_shooterJoyStick.getY());
+			
+			//m_shooter.arcadeDrive(m_shooterJoyStick.getY(),m_shooterJoyStick.getX());
+			
+			if(m_shooterJoyStick.getRawButton(ButtonMap.Y))
+			{
+				m_intake.set(1.0);
+			} else {
+				m_intake.set(0.0);
+			} 
 			
 			// The motors will be updated every 5ms
 			Timer.delay(0.005);
